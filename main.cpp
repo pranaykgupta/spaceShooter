24626d6e1481c78466a8e0726c5e09dfc3cc81f5
pranaykgupta <43784050+pranaykgupta@users.noreply.github.com>
2018-10-02 20:30:19 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
-#include <cstdlib>
-using namespace std;
+#include <ctype.h>
+#include <string.h>
+#include<stdio.h>
+int main ()
+{using namespace std;
+    string ch1,ch2;
+    cin>>ch1>>ch2;
+    int len = ch1.len();
+    int flag =0;
+    for(int i=0;i<len;i++)
+    {
+        if (isupper(ch1[i])==0)
+            tolower(ch1[i]);
+            if (isupper(ch2[i])==0)
+            tolower(ch2[i]);
 
 
-int main()
-{
-    for(int i=1;i<=25;i++)
-    cout<<1+rand()%6<<endl;
+    }
+   for (int i=0;i<len;i++)
+   {
+       if(ch1[i]<ch2[i])
+      {
+
+       cout<<"-1";
+         flag == 1;
+       break;}
+       if (ch1[i]>ch2[i])
+       {
+
+        cout <<"1";
+       flag == 1;
+       break;}
+   }
+   if (flag ==0)
+    cout<<"0";
+
+    return 0;
 }
+
